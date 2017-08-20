1748c0e6593c696861fb47a465d02473b7c07a55
JuneBuug <junee613@gmail.com>
2017-08-21 01:03:49 +0900

    Product : 브랜드 카테고리 필터링과 가격순 정렬

--- cs/views.py
+++ cs/views.py
@@ -1,3 +1,5 @@
+from django.db.models import IntegerField
+from django.db.models.functions import Cast
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product
@@ -23,7 +25,29 @@ class product_api(GenericAPIView, mixins.ListModelMixin):
 
     def get_queryset(self):
         queryset = Product.objects.all()
+
+        order = self.request.query_params.get('order', None)
+        if order is not None:
+            if order == "1":
+                queryset = Product.objects.extra(
+                    select={'myinteger': "CAST(substring(price FROM '^[0-9]+') AS INTEGER)"}
+                ).order_by('myinteger')
+
         name = self.request.query_params.get('name',None)
         if name is not None :
             queryset = queryset.filter(name=name)
+
+        brand = self.request.query_params.get('brand',None)
+        if brand is not None:
+            queryset = queryset.filter(brand=brand)
+
+        category = self.request.query_params.get('category', None)
+        if category is not None:
+            queryset = queryset.filter(category=category)
+
+        # event = self.request.query_params.get('event', None)
+        # if event is not None:
+        #     queryset = queryset.filter(data__event=event)
+        # 왜 안될까
+
         return queryset
