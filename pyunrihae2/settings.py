c694c4c91f966a50fefca538f57c3e5bf3ab4415
JuneBuug <junee613@gmail.com>
2017-08-21 12:35:50 +0900

    fabfile 추가

--- pyunrihae2/settings.py
+++ pyunrihae2/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -91,7 +92,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pyunrihae',
-        'USER': '',
+        'USER': 'june',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
